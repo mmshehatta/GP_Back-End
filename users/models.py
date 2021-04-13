07a93bc5b17a3df6c05abc0939253a80e48389ea
mmshehatta <mmshehatt@gmail.com>
2021-04-13 16:38:40 +0200

    Adding user App implementation

--- users/models.py
+++ users/models.py
@@ -1,22 +1,22 @@
 
-# from django.db import models
-# from phone_field import PhoneField
-# from django.contrib.auth.models import User
-# from django.db.models.signals import post_save
+from django.db import models
+from phone_field import PhoneField
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# # Create your models here.
+# Create your models here.
 
-# class UserProfile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=100 , null=False , blank=False , unique=False)
-#     phone = PhoneField(blank=True, help_text='Contact phone number')
-#     email = models.EmailField(max_length = 254)
-#     image = models.ImageField(upload_to='UserProfile/images' , null=True , blank=True)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100 , null=False , blank=False , unique=False)
+    phone = PhoneField(blank=True, help_text='Contact phone number')
+    email = models.EmailField(max_length = 254)
+    image = models.ImageField(upload_to='users/UserProfile/images' , null=True , blank=True)
 
-#     def _str_(self):
-#         return '%s' % self.user
+    def __str__(self):
+        return '%s' % self.user
 
-#     def create_profile(sender, **kwargs):
-#         if kwargs['created']:
-#             user_profile = UserProfile.objects.create(user=kwargs['instance'])
-#     post_save.connect(create_profile, sender=User)
+    def create_profile(sender, **kwargs):
+        if kwargs['created']:
+            user_profile = UserProfile.objects.create(user=kwargs['instance'])
+    post_save.connect(create_profile, sender=User)
