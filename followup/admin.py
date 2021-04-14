046b2a77fb210dd3860f0ec2cc05767ae322d4d8
amlshohdy <amlshohdy667@gmail.com>
2021-04-14 16:27:23 +0200

    followup

--- followup/admin.py
+++ followup/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Questions,ContactUs ,Reviews
 
 # Register your models here.
+admin.site.register(Questions)
+admin.site.register(ContactUs)
+admin.site.register(Reviews)
